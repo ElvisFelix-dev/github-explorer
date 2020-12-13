83e576c1a2c32312f4e290de3c3eec967dffbe62
Elvis Felix <elvisjaspion@gmail.com>
2020-12-13 17:17:39 -0300

    Lidando com Error

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Daschboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+    <Switch>
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/repository" component={Repository} />
+    </Switch>
+)
+
+export default Routes;
